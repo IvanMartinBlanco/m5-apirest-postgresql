13854816364862ffd80fdeb8bae9562625157de8
Ivan <ivanmartinblanco1985@gmail.com>
2021-07-29 10:02:50 +0200

    Datos inicio

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO cars (manufacturer, model, cc, doors) VALUES ('Ford', 'Mondeo', '333', '3');
+INSERT INTO cars (manufacturer, model, cc, doors) VALUES ('Seat', 'Cordoba', '333', '3');
+INSERT INTO cars (manufacturer, model, cc, doors) VALUES ('Tesla', 'V333', '333', '3');
